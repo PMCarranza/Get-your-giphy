5200f18335f02d760837f304c89f2dde8240b109
Marino Carranza <marino.carranza@gmail.com>
2019-07-30 18:50:49 -0700

    finishes notes on js file

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var animals = [
     "capybara", "teacup pig", "serval", "salamander", "frog", "chupacabra", "leon", "foca", "tigre", "quetzal"
 ];
 
-    // function populateButtons is defined and three parameters are assigne to it
+    // function populateButtons is dclared and three parameters are assigne to it
 function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
     
         // method empty() is being used to empty the contents of areaToAdd, this will be done every time the code runs before any other content is added, clearing the way for a new set of pictures to be added to the dom
@@ -90,13 +90,13 @@ function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
         // an image tag is going to be created using jquery and it is going to be passed to the variable animalImage
             var animalImage = $("<img>");
             
-        // variable animalImage being passed the attributes of source and the contents of the variable still, each time a button is clicked this will display the first ten images in the promise from line 67
+        // variable animalImage is being passed the attributes of source and the contents of the variable still, each time a button is clicked this will display the first ten still images in the promise from line 67
             animalImage.attr("src", still);
             
-        // animalImage is being assigned the attribute of "data-still" and the values of the variable still, in this case the still image fo the gif will be displayed
+        // animalImage is being assigned the attribute of "data-still" and the values of the variable still, in this case the still image of the gif will be displayed, this is being done in pairs (attribute/value)
             animalImage.attr("data-still", still);
             
-        // animalImage is being assigned the atribute of "data-state", and the values of the variable animated which contains the url for the "moving" gif
+        // animalImage is being assigned the atribute of "data-animate", and the values of the variable animated which contains the url for the "moving" gif (as a pair)
             animalImage.attr("data-animate", animated);
             
         // 
@@ -117,31 +117,62 @@ function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
     });  // closes the .then method and the promise
 }); //closes on click and its function
 
-$(document).on("click", ".animal-image", function() {
-
+    // the on() method is attaching an event function to the jquery object, in this case the event is a click to the class animal-image button; these are the images displayed on the screen
+    $(document).on("click", ".animal-image", function () {
+    
+    // the attribute of data-state is being passed to (this) which in turn is being assigned to the variable state, the attribute changes on click from still to animate and viceversa 
     var state = $(this).attr("data-state");
 
+    // this condition compares the value of the variable state to the value attribute of animalImage (line 103)  
     if (state === "still") {
-    $(this).attr("src", $(this).attr("data-animate"));
-    $(this).attr("data-state", "animate");
-    }
+
+    // if the condition proves true then the value of (this) changes its attribute to "data-animate" and this contains the value of animated (line 100)
+    $(this).attr("src", $(this).attr("data-animate"));    
+    
+    //the data-state attribute contains the value of animate and both values (as a pair) are being added to (this)
+        $(this).attr("data-state", "animate");
+    }   //closes if statement
+    // if the condition proves false the code after the else will run
     else {
-    $(this).attr("src", $(this).attr("data-still"));
-    $(this).attr("data-state", "still");
-    }
-});
+        // (this) is given the attribute of "src" (which changes on click) and the value of this which at the same time has the attribute of "data-still", this feel a lot more than value pairs
+        $(this).attr("src", $(this).attr("data-still"));
+        
+        //the data-state attribute contains the value of still and both values (as a pair) are being added to (this)
+        $(this).attr("data-state", "still");
+    }   //closes else statement
+}); // closes on click event and its function
 
-$("#add-animal").on("click", function(event) {
+// the on() method is attaching an event function to the div with the id add-animal, in this case the event is a click
+$("#add-animal").on("click", function (event) {
+    
+    // the preventDefaul method can be used because "click" event is a cancelable.
+    // this method stops the default behavior of the click will to occur
+    // oin this case the method is preventing the click from appending the new animal button to the div with the id animal-buttons
     event.preventDefault();
-    var newAnimal = $("input").eq(0).val();
+    
+    // variable newAnimal is being declared
 
+    // the value of input is being captured and passed to the variable newAnimal via the eq() method.
+
+    // Given a jQuery object that represents a set of DOM elements, the .eq() method constructs a new jQuery object from one element within that set. The supplied index identifies the position of this element in the set.  (from the aQuery API documentation) 
+
+    // in my own words the eq() is a 0 based index method that adds a new object to the dom, (sounds like append), the index is used to tell the method where to add the new object, some documentation I have found say it starts from the back some other explains it as if it were an array starting at the front (more reading being done).  In this case the input is being added to the end of the array of animals and the index of eq() is 0 but my guess .push() in line 167 is the culprit.
+    // the index selects the element within the matched set.
+    var newAnimal = $("input").eq(0).val();
+    
+    //this conditional prevents two letter words from being added to the div animal-buttons
     if (newAnimal.length > 2) {
+    
+    //the new animal with at least three letters is being push to the end of the array animals
     animals.push(newAnimal);
     }
-
+    // function populateButtons is being called and the parameters parameters being fed to it are animals (the variable containing the array of animal categories), "animal-button" (the class added to variable a which is the button), "animals-buttons" (is the div with the buttons for all the categories) 
+    // this is being called here to populate the new animal
     populateButtons(animals, "animal-button", "#animal-buttons");
 
-});
+}); //closes click on div with id add-animal and its function
 
+// function populateButtons is being called and the parameters parameters being fed to it are animals (the variable containing the array of animal categories), "animal-button" (the class added to variable a which is the button), "animals-buttons" (is the div with the buttons for all the categories)
+// this is being called here to populate on load
 populateButtons(animals, "animal-button", "#animal-buttons");
-});
+}); //closes document ready function
